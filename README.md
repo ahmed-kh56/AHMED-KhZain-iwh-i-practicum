4de3f51bde776cc80e701c133d78013235d50e17
Ahmed Zain <eltwo3m@gmail.com>
2025-11-03 16:43:22 +0200

    Done

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+
+## Custom Object List View
+
+You can view the custom object list on HubSpot here:
+
+https://app.hubspot.com/contacts/147185116/objects/2-194143844/views/all/list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Welcome to the Integrating With HubSpot I: Foundations Practicum
 
 This repository is for the Integrating With HubSpot I: Foundations course. This practicum is one of two requirements for receiving your Integrating With HubSpot I: Foundations certification. You must also take the exam and receive a passing grade (at least 75%).
--- index.js
+++ index.js
@@ -1,26 +1,73 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// 🏠 ROUTE 1 - Homepage: عرض كل الكستم أوبجكتات
+app.get('/', async (req, res) => {
+  const url = 
+    'https://api.hubapi.com/crm/v3/objects/2-194143844?properties=name&properties=type&properties=age';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Code for Route 1 goes here
+  try {
+    const response = await axios.get(url, { headers });
+    const data = response.data.results.map(obj => ({
+      id: obj.id,
+      name: obj.properties.name,
+      type: obj.properties.type,
+      age: obj.properties.age
+    }));
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+    res.render('homepage', { title: 'My Custom Objects | HubSpot Practicum', data });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send('Error fetching data');
+  }
+});
 
-// * Code for Route 2 goes here
+// 📝 ROUTE 2 - صفحة الفورم لإضافة أو تحديث كستم أوبجكت
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | HubSpot Practicum' });
+});
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+// 🚀 ROUTE 3 - POST لإنشاء كستم أوبجكت جديد
+app.post('/update-cobj', async (req, res) => {
+  const url = 'https://api.hubapi.com/crm/v3/objects/2-194143844';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Code for Route 3 goes here
+  const newObj = {
+    properties: {
+      name: req.body.name,
+      type: req.body.type,
+      age: req.body.age
+    }
+  };
+
+  try {
+    await axios.post(url, newObj, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send('Error adding new record');
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -67,5 +114,4 @@ app.post('/update', async (req, res) => {
 */
 
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+
